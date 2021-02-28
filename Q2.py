697f84b8d8f4ec852310de58b2a59549133260bf
Devasheesh Mishra <72092910+devasheeshG@users.noreply.github.com>
2021-03-01 00:04:54 +0530

    .

---
+++ Q2.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if(num1>num2):
+    print(num1, "is the largest integer")
+elif(num2>num3):
+    print(num2, "is the largest integer")
+else:
+    print(num3, "is the largest integer")
---
+++ Q3.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a number : ")
+r=int(input("Enter range of table : ")
+for i in range(0,r):
+      i=i+1
+      print(i)
