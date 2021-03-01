dcc05ad928c42fd3b90061bc8f1a195ca9690e96
Devasheesh Mishra <72092910+devasheeshG@users.noreply.github.com>
2021-03-01 13:24:13 +0530

    .

--- Q3.py
+++ Q3.py
@@ -1,5 +1,8 @@
-n=int(input("Enter a number : ")
-r=int(input("Enter range of table : ")
+n=int(input("Enter a number : "))
+r=int(input("Enter range of table : "))
+print("Multiplication Table of",n,"is")
 for i in range(0,r):
       i=i+1
-      print(i)
+      print(n,"X",i,"=",n*i)
+
+print("Loop completed")
